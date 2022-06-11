fa2b1fd7afeb48aaa8612068967c0bff54b7ebd0
Garance Clausse <garanceclausse@msn.com>
2022-06-11 11:52:54 +0200

    ex00 done without makefile

---
+++ ex00/megaphone.cpp
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gclausse <gclausse@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/11 11:22:48 by gclausse          #+#    #+#             */
+/*   Updated: 2022/06/11 11:22:48 by gclausse         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <cctype>
+
+int	main(int argc, char **argv)
+{
+	int	i;
+	int	j;
+
+	i = 1;
+	if (argc == 1)
+	{
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+		return (0);
+	}
+	else
+	{
+		while (argv[i])
+		{
+			j = 0;
+			while (argv[i][j])
+			{
+				char ch = toupper(argv[i][j]);
+				
+ 				std::cout << ch;
+				j++;
+			}
+			i++;
+		}
+		std::cout<< " " << std::endl;
+	}
+	return (0);
+}
