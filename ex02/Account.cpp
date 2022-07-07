264a96150872def864f7ce1342b7415c8915357c
GaranceClausse <garanceclausse@msn.com>
2022-07-07 18:53:18 +0200

    Initial commit

---
+++ ex02/Account.cpp
@@ -0,0 +1,61 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Account.cpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gclausse <gclausse@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/07 18:49:02 by gclausse          #+#    #+#             */
+/*   Updated: 2022/07/07 18:51:47 by gclausse         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+static int	getNbAccounts( void )
+{
+
+}
+
+static int	getTotalAmount( void )
+{
+
+}
+
+static int	getNbDeposits( void )
+{
+
+}
+
+static int	getNbWithdrawals( void )
+{
+
+}
+
+static void	displayAccountsInfos( void )
+{
+
+}
+
+void	makeDeposit( int deposit )
+{
+
+}
+
+bool	makeWithdrawal( int withdrawal )
+{
+
+}
+
+int		checkAmount( void ) const
+{
+
+}
+
+void	displayStatus( void ) const
+{
+
+}
+
+static void	_displayTimestamp( void )
+{
+	
+}
